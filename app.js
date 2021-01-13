4d40c420997869a93d0f6cd9d74598382e76f527
Paulo Martin <paumar559@gmail.com>
2021-01-12 21:21:28 -0800

    New Push

--- app.js
+++ app.js
@@ -16,7 +16,8 @@
             // Question 1
             var questionOne = prompt("1. Did I serve the military? yes/no")
             var responseOne = questionOne.toUpperCase()
-           
+            console.log(responseOne)
+
             if (responseOne === 'YES') {
                 alert('CORRECT!') ;
             } else {
@@ -26,7 +27,8 @@
             // Question 2
             var questionTwo = prompt("2. Did I lived in Japan to trade bitcoins? yes/no")
             var responseTwo = questionTwo.toUpperCase()
-           
+            console.log(responseTwo)
+
             if (responseTwo === 'NO') {
                 alert('CORRECT! I lived in the Philippines!') ;
             } else {
@@ -36,7 +38,8 @@
             // Question 3
             var questionThree = prompt("3. I would go to startup hackathons to work on Mesh Networking projects? yes/no")
             var responseThree = questionThree.toUpperCase()
-           
+            console.log(responseThree)
+
             if (responseOne === 'YES') {
                 alert('CORRECT!') ;
             } else {
@@ -46,7 +49,8 @@
             // Question 4
             var questionFour = prompt("4. Did I worked in Outdoor Recreation Program helping Veterans transition to civilian life after the military? yes/no")
             var responseFour = questionFour.toUpperCase()
-           
+            console.log(responseFour)
+
             if (responseFour === 'YES') {
                 alert('CORRECT!') ;
             } else {
@@ -56,7 +60,9 @@
             // Question 5
             var questionFive = prompt("I'm building a Van so I can travel the US and code along the way to Texas! yes/no")
             var responseFive = questionFive.toUpperCase()
-           
+            console.log(responseFive)
+
+
             if (responseFive === 'YES') {
                 alert('CORRECT!') ;
             } else {
