ee20eb8b177cdae58f739fae0d46fa9c57243fb7
Paulo Martin <paumar559@gmail.com>
2021-01-12 20:59:36 -0800

    Lab 2 Push

---
+++ app.js
@@ -0,0 +1,69 @@
+'use strict';
+
+            // Code Block 1
+            // Get data from the user
+            var userName = prompt('What is your name?');
+            var greeting = 'Youre name is: ';
+       
+            // display data to the user
+            alert(greeting + userName);
+       
+            // log data to your developer console
+            console.log('The users name is ' + userName);
+       
+            //////////////////////////////////////////////////////
+
+            // Question 1
+            var questionOne = prompt("1. Did I serve the military? yes/no")
+            var responseOne = questionOne.toUpperCase()
+           
+            if (responseOne === 'YES') {
+                alert('CORRECT!') ;
+            } else {
+                alert('INCORRECT');
+            }
+
+            // Question 2
+            var questionTwo = prompt("2. Did I lived in Japan to trade bitcoins? yes/no")
+            var responseTwo = questionTwo.toUpperCase()
+           
+            if (responseTwo === 'NO') {
+                alert('CORRECT! I lived in the Philippines!') ;
+            } else {
+                alert('INCORRECT');
+            }
+
+            // Question 3
+            var questionThree = prompt("3. I would go to startup hackathons to work on Mesh Networking projects? yes/no")
+            var responseThree = questionThree.toUpperCase()
+           
+            if (responseOne === 'YES') {
+                alert('CORRECT!') ;
+            } else {
+                alert('INCORRECT');
+            }
+
+            // Question 4
+            var questionFour = prompt("4. Did I worked in Outdoor Recreation Program helping Veterans transition to civilian life after the military? yes/no")
+            var responseFour = questionFour.toUpperCase()
+           
+            if (responseFour === 'YES') {
+                alert('CORRECT!') ;
+            } else {
+                alert('INCORRECT');
+            }
+
+            // Question 5
+            var questionFive = prompt("I'm building a Van so I can travel the US and code along the way to Texas! yes/no")
+            var responseFive = questionFive.toUpperCase()
+           
+            if (responseFive === 'YES') {
+                alert('CORRECT!') ;
+            } else {
+                alert('INCORRECT');
+            }
+            
+
+
+
+        
